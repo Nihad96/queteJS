eaf9fc8a001bc546e956b22417295931f6659757
Nihad96 <nihad.zatric@gmail.com>
2018-12-17 15:06:53 +0100

    fini

---
+++ script.js
@@ -0,0 +1,8 @@
+// Define some variables
+const name = 'Pulp Fiction';
+const year = '1994';
+const director = 'Quentin Tarantino'
+const message = 'My favourite movie is ' + name + '! It\'s been directed in ' + year + ' by ' + director + '!';
+
+// Display a popup
+alert(message);
